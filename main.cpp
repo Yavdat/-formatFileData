05441f2fe8466caff15e171254e4490cd99bf817
Yavdat <yavdat.it@gmail.com>
2022-12-19 07:21:47 +0300

    add input.ignore

--- main.cpp
+++ main.cpp
@@ -6,18 +6,27 @@ using namespace std;
 
 int main() {
     ifstream input("data.txt");
-    string year;
-    string month;
-    string day;
+    int year;
+    int month;
+    int day;
     // is_open необязателен
     if (input) {
         // getline(input, line);
-        while (getline(input, year, '-')) {
-            cout << year << endl;
-            getline(input, month, '-');
-            cout << month << endl;
-            getline(input, day);
-            cout << day << endl;
+        // while (getline(input, year, '-')) {
+        //     cout << year << endl;
+        //     getline(input, month, '-');
+        //     cout << month << endl;
+        //     getline(input, day);
+        //     cout << day << endl;
+        // }
+        while(input) {
+            input >> year;
+            input.ignore(1);
+            input >> month;
+            input.ignore(1);
+            input >> day;
+            input.ignore(1);
+            cout << year << ' ' << month << ' ' << day << endl;
         }
     } else {
         cout << "error" << endl;
