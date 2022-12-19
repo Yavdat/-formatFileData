fa6b3cf58bd02780041b4936df203cf0903e6f2e
Yavdat <yavdat.it@gmail.com>
2022-12-19 07:49:00 +0300

    using iomanip

--- main.cpp
+++ main.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <iomanip>
 
 using namespace std;
 
+void ReadAll(const string& path) {
+    ifstream input(path);
+    if (input) {
+        string line;
+        while (getline(input, line)) {
+            cout << line << endl;
+        }
+    }
+}
+
 int main() {
-    ifstream input("data.txt");
+    const string path = "data.txt";
+    ifstream input(path);
     int year;
     int month;
     int day;
     // is_open необязателен
     if (input) {
-        // getline(input, line);
-        // while (getline(input, year, '-')) {
-        //     cout << year << endl;
-        //     getline(input, month, '-');
-        //     cout << month << endl;
-        //     getline(input, day);
-        //     cout << day << endl;
-        // }
         while(input) {
             input >> year;
             input.ignore(1);
@@ -31,5 +36,20 @@ int main() {
     } else {
         cout << "error" << endl;
     }
+
+    vector<string> names = {"a", "b", "c"};
+    vector<double> values = {5, 0.01, 0.000005};
+    cout << setfill('.'); // заполняем отступы точками
+    cout << left; // выравнивание по левому краю
+
+    for (const auto& n : names) {
+        cout << setw(10) << n << ' ';
+    }
+    cout << endl;
+    cout << fixed << setprecision(2); // убираем экспоненциальный формат, и задаем точность
+    for (const auto& v : values) {
+        cout << setw(10) << v << ' ';
+    }
+    cout << endl;
     return 0;
 }
